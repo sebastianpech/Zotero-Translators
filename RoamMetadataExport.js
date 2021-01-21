0f2453af75750ad3ba9eac149077529bab43b35a
Sebastian Pech <sebastian.pech@me.com>
2021-01-21 13:32:40 +0100

    Fix for web articles

--- RoamMetadataExport.js
+++ RoamMetadataExport.js
@@ -18,28 +18,26 @@
 function doExport() {
   var item;
   while (item = Zotero.nextItem()) {
+  if (item.creators.length > 0) {
     var creatorsS = item.creators[0].lastName;
-    if (item.creators.length>2) {
+    if (item.creators.length > 2) {
       creatorsS += " et al.";
-    } else if (item.creators.length==2) {
-      creatorsS += " & " + item.creators[1].lastName;    
+    } else if (item.creators.length == 2) {
+      creatorsS += " & " + item.creators[1].lastName;
     }
+  }
 
-    var citationKey = (item.citationKey) ? "@"+item.citationKey : "(bib citkey missing)";
-    Zotero.write('[[' + citationKey + ']]\n');
-
+  if (item.creators.length > 0) {
     Zotero.write('  author:: ');
-
-    for (author in item.creators){
+    for (author in item.creators) {
       if (item.creators[author].firstName !== undefined) {
         Zotero.write('[[' + item.creators[author].firstName + ' ' + item.creators[author].lastName + ']] ');
       } else {
         Zotero.write('[[' + item.creators[author].lastName + ']] ');
-
       }
     }
-
     Zotero.write('\n');
+  }
 
     var titleS = (item.title) ? item.title : "(no title)";
     titleS = titleS.replace('<i>', '__')
@@ -59,6 +57,10 @@ function doExport() {
     Zotero.write('  year:: ')
     Zotero.write(dateS + '\n')
     
+  if (!(["book", "bookSection", "journalArticle"].includes(item.itemType))) {
+    Zotero.write('  Access date:: ')
+    Zotero.write(item.accessDate + '\n')
+  }
     if (item.url !== undefined) {
       Zotero.write('  url:: [')
       Zotero.write(creatorsS)
