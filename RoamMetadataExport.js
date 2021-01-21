b8870b7a021775b52aa711853ab2ba6e29f9abcc
Sebastian Pech <sebastian.pech@me.com>
2021-01-21 13:36:47 +0100

    Fix indentation

--- RoamMetadataExport.js
+++ RoamMetadataExport.js
@@ -18,29 +18,29 @@
 function doExport() {
   var item;
   while (item = Zotero.nextItem()) {
-  if (item.creators.length > 0) {
-    var creatorsS = item.creators[0].lastName;
-    if (item.creators.length > 2) {
-      creatorsS += " et al.";
-    } else if (item.creators.length == 2) {
-      creatorsS += " & " + item.creators[1].lastName;
-    }
-  }
-
-  var citationKey = (item.citationKey) ? "@"+item.citationKey : "(bib citkey missing)";
-    Zotero.write('[[' + citationKey + ']]\n');
-
-  if (item.creators.length > 0) {
-    Zotero.write('  author:: ');
-    for (author in item.creators) {
-      if (item.creators[author].firstName !== undefined) {
-        Zotero.write('[[' + item.creators[author].firstName + ' ' + item.creators[author].lastName + ']] ');
-      } else {
-        Zotero.write('[[' + item.creators[author].lastName + ']] ');
+    if (item.creators.length > 0) {
+      var creatorsS = item.creators[0].lastName;
+      if (item.creators.length > 2) {
+        creatorsS += " et al.";
+      } else if (item.creators.length == 2) {
+        creatorsS += " & " + item.creators[1].lastName;
       }
     }
-    Zotero.write('\n');
-  }
+
+    var citationKey = (item.citationKey) ? "@" + item.citationKey : "(bib citkey missing)";
+    Zotero.write('[[' + citationKey + ']]\n');
+
+    if (item.creators.length > 0) {
+      Zotero.write('  author:: ');
+      for (author in item.creators) {
+        if (item.creators[author].firstName !== undefined) {
+          Zotero.write('[[' + item.creators[author].firstName + ' ' + item.creators[author].lastName + ']] ');
+        } else {
+          Zotero.write('[[' + item.creators[author].lastName + ']] ');
+        }
+      }
+      Zotero.write('\n');
+    }
 
     var titleS = (item.title) ? item.title : "(no title)";
     titleS = titleS.replace('<i>', '__')
@@ -49,46 +49,46 @@ function doExport() {
     Zotero.write('  title:: ')
     Zotero.write(titleS + '\n')
 
-    if(item.publicationTitle !== undefined) {
-      Zotero.write('  publication:: ') 
+    if (item.publicationTitle !== undefined) {
+      Zotero.write('  publication:: ')
       Zotero.write(item.publicationTitle + '\n')
     }
-    
+
     var date = Zotero.Utilities.strToDate(item.date);
-    var dateS = (date.year) ? date.year : item.date;   
+    var dateS = (date.year) ? date.year : item.date;
 
     Zotero.write('  year:: ')
     Zotero.write(dateS + '\n')
-    
-  if (!(["book", "bookSection", "journalArticle"].includes(item.itemType))) {
-    Zotero.write('  Access date:: ')
-    Zotero.write(item.accessDate + '\n')
-  }
+
+    if (!(["book", "bookSection", "journalArticle"].includes(item.itemType))) {
+      Zotero.write('  Access date:: ')
+      Zotero.write(item.accessDate + '\n')
+    }
     if (item.url !== undefined) {
       Zotero.write('  url:: [')
       Zotero.write(creatorsS)
       Zotero.write(' (' + dateS + '). ')
       titleS = titleS.replace(/__/g, "")
       Zotero.write(titleS + '.')
-      if(item.publicationTitle !== undefined) {
+      if (item.publicationTitle !== undefined) {
         Zotero.write(' ' + item.publicationTitle)
       }
       Zotero.write('](' + item.url + ')\n')
     }
 
 
-    var library_id = item.libraryID ? item.libraryID : 0;  
+    var library_id = item.libraryID ? item.libraryID : 0;
     var itemLink = 'zotero://select/items/' + library_id + '_' + item.key;
 
     Zotero.write('  Zotero link:: ')
     Zotero.write('[Zotero Link](' + itemLink + ')\n')
 
     // Zotero.write('  topics:: \n')
-    
+
     Zotero.write('  tags:: #view-later\n')
 
     // Zotero.write('  citation graph:: {{[[query]]: {and: [[quote]] [[citing]] [['+citationKey+']] {not: [[query]]}}}}\n')
-    
+
 
   }
 }
